f075358b7391f32b7f671056ab221d59f3a8abd7
Isaladwy <Isaladwy@gmail.com>
2025-07-20 16:27:01 +0300

    Add apiResponse object and localUser assignment in index.ts

--- index.ts
+++ index.ts
@@ -3,3 +3,13 @@ let username = 'Isaladwy';
   username = '10'
 
   console.log(username)
+
+  const apiResponse = {
+    id:1,
+    name: 'Isaladwy',
+    birthDate: '1990-01-01',
+  }
+
+  const localUser = apiResponse
+
+  localUser.id = 2
