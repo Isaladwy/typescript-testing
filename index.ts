f77cee1a6909978d98c459a7cf1de3ae57da81c6
Isaladwy <Isaladwy@gmail.com>
2025-08-19 11:00:07 +0300

    Synchronous (Blocking) language with Asynchronous (Non-Bloking) Capabilities

--- index.ts
+++ index.ts
@@ -1,123 +1,141 @@
 let username = 'Isaladwy';
 
-  username = '10'
+username = '10';
 
-  console.log(username)
+console.log(username);
 
-  const apiResponse = {
-    id:1,
-    name: 'Isaladwy',
-    birthDate: '1990-01-01',
-  }
+const apiResponse = {
+  id: 1,
+  name: 'Isaladwy',
+  birthDate: '1990-01-01',
+};
 
-  const localUser = apiResponse
+const localUser = apiResponse;
 
-  localUser.id = 2
+localUser.id = 2;
 
-  //////////////
+//////////////
 
-  let num1: number = 20;
-  let num2: number = 7;
+let num1: number = 20;
+let num2: number = 7;
 
-  const sum: number = num1 + num2;
-  console.log(sum  )
-  
-  const difference: number = num1 - num2;
-  console.log(difference)
+const sum: number = num1 + num2;
+console.log(sum);
 
-  const multi: number = num1 * num2;
-  console.log(multi)
+const difference: number = num1 - num2;
+console.log(difference);
 
-  const division: number = num1 / num2;
-  console.log(division)
+const multi: number = num1 * num2;
+console.log(multi);
 
-  const reminder: number = num1 % num2;
-  console.log(reminder)
+const division: number = num1 / num2;
+console.log(division);
 
-  //////////////
+const reminder: number = num1 % num2;
+console.log(reminder);
 
-  console.log(5 == '5') // loose equality (Value comparison)
-  console.log(5 === '5') // strict equality (Value and Type comparison)
-  console.log(5 != '5') // loose inequality (Value comparison)
-  console.log(5 !== '5') // strict inequality (Value and Type comparison)
+//////////////
 
-  console.log(num1 < num2) // less than
-  console.log(num1 > num2) // greater than
-  console.log(num1 <= num2) // less than or equal to
-  console.log(num1 >= num2) // greater than or equal to
+console.log(5 == '5'); // loose equality (Value comparison)
+console.log(5 === '5'); // strict equality (Value and Type comparison)
+console.log(5 != '5'); // loose inequality (Value comparison)
+console.log(5 !== '5'); // strict inequality (Value and Type comparison)
 
-  //////////////
+console.log(num1 < num2); // less than
+console.log(num1 > num2); // greater than
+console.log(num1 <= num2); // less than or equal to
+console.log(num1 >= num2); // greater than or equal to
 
-  const x: boolean = true;
-  const y: boolean = false;
+//////////////
 
-  console.log(x && y) // logical AND
-  console.log(x || y) // logical OR
-  console.log(!x) // logical NOT
-  console.log(x && !y) // logical AND with NOT
-  console.log(!x && y) // logical NOT with AND
-  console.log(x || !y) // logical OR with NOT
-  console.log(!x || y) // logical NOT with OR
-  console.log(x && y || !y) // complex logical expression
+const x: boolean = true;
+const y: boolean = false;
+
+console.log(x && y); // logical AND
+console.log(x || y); // logical OR
+console.log(!x); // logical NOT
+console.log(x && !y); // logical AND with NOT
+console.log(!x && y); // logical NOT with AND
+console.log(x || !y); // logical OR with NOT
+console.log(!x || y); // logical NOT with OR
+console.log((x && y) || !y); // complex logical expression
 
 ////////////////
 
-const person ={
+const person = {
   username: 'Isaladwy',
   age: 30,
-  eat: function(){
-    return `I'm eating now`
+  eat: function () {
+    return `I'm eating now`;
   },
-  walk(){
-    return `I'm walking now`
-  }
-}
+  walk() {
+    return `I'm walking now`;
+  },
+};
 
-const {eat, walk} = person
+const { eat, walk } = person;
 
-console.log(person.eat())
-console.log(person.walk())
+console.log(person.eat());
+console.log(person.walk());
 
-const islam = {...person, username: 'Tamim', age: 2.5}
+const islam = { ...person, username: 'Tamim', age: 2.5 };
 
-console.log(islam.eat())
-console.log(islam)
+console.log(islam.eat());
+console.log(islam);
 
 //////////////// Higher Order Functions
 
-const numbers: number[] = [1,2,3,4,5];
+const numbers: number[] = [1, 2, 3, 4, 5];
 
-const squaredNumbers = numbers.map(number => number * number);
+const squaredNumbers = numbers.map((number) => number * number);
 
-const evenNumbers = numbers.filter(number => number % 2 === 0);
+const evenNumbers = numbers.filter((number) => number % 2 === 0);
 
-const foundNumber = numbers.find(number => number === 4);
+const foundNumber = numbers.find((number) => number === 4);
 
-const hasNegative = numbers.some(number => number < 0);
+const hasNegative = numbers.some((number) => number < 0);
 
-const allPositive = numbers.every(number => number >= 0);
+const allPositive = numbers.every((number) => number >= 0);
 
-const totalSum = numbers.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+const totalSum = numbers.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  0
+);
 
 console.log(squaredNumbers);
 console.log(evenNumbers);
 console.log(foundNumber);
-console.log(hasNegative); 
+console.log(hasNegative);
 console.log(allPositive);
 console.log(totalSum);
 
 ////////////////
 
-
 const person2 = {
   username: 'Codeawy',
-  walk(){
+  walk() {
     console.log(this);
     return "I'm walking now";
   },
 };
 
-function printThis(this: unknown){
+function printThis(this: unknown) {
   return this;
 }
 console.log(printThis());
+
+///Synchronous (Blocking) language with Asynchronous (Non-Bloking) Capabilities///
+
+function first() {
+  console.log('First Function');
+}
+function second() {
+  console.log('Second Function');
+}
+function third() {
+  console.log('Third Function');
+}
+
+first();
+second();
+third();
